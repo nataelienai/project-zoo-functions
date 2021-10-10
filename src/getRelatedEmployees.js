4e812f12f3270c88da8cf6d95aa4468814ced269
Natã Elienai <nata.elienai@gmail.com>
2021-10-10 20:34:34 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const { employees } = data;
+  const employeeFound = employees.find(({ managers }) => managers.includes(id));
+
+  return employeeFound !== undefined;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const { employees } = data;
+
+  return employees
+    .filter(({ managers }) => managers.includes(managerId))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
