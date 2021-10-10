07cbb30f337ccebadbc4292767fe60e8d7bd06bf
Natã Elienai <nata.elienai@gmail.com>
2021-10-10 18:05:58 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const { residents } = species.find(({ name }) => animal === name);
+
+  return residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
