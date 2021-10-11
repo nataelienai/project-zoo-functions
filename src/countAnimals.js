92a3bc04b69b3a89dbf15ffedb454451f03e1a92
Natã Elienai <nata.elienai@gmail.com>
2021-10-10 21:17:40 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
+function countAllSpecies() {
+  const { species } = data;
+
+  return species.reduce((animalCount, { name, residents }) => (
+    {
+      ...animalCount,
+      [name]: residents.length,
+    }
+  ), {});
+}
+
+function countSpecies({ specie, sex }) {
+  const { species } = data;
+  const { residents } = species.find(({ name }) => name === specie);
+
+  if (sex === undefined) {
+    return residents.length;
+  }
+  return residents
+    .filter(({ sex: residentSex }) => residentSex === sex)
+    .length;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return countAllSpecies();
+  }
+  return countSpecies(animal);
 }
 
 module.exports = countAnimals;
