3cc887b625dd2bd6d59d99bd4e20536a68f757b7
Natã Elienai <nata.elienai@gmail.com>
2021-10-11 16:24:41 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce(({ child, adult, senior }, entrant) => {
+    if (entrant.age < 18) {
+      return { child: child + 1, adult, senior };
+    }
+    if (entrant.age < 50) {
+      return { child, adult: adult + 1, senior };
+    }
+    return { child, adult, senior: senior + 1 };
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { prices } = data;
+  const entrantCount = countEntrants(entrants);
+
+  return (prices.child * entrantCount.child)
+  + (prices.adult * entrantCount.adult)
+  + (prices.senior * entrantCount.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
