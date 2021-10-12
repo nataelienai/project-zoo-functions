ac6efa74c31c04924cd819de86b496999abdc31a
Natã Elienai <nata.elienai@gmail.com>
2021-10-12 16:45:47 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,57 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeeById(id) {
+  return employees.find((employee) => employee.id === id);
+}
+
+function getEmployeeByName(name) {
+  return employees
+    .find(({ firstName, lastName }) => firstName === name || lastName === name);
+}
+
+function getEmployee({ name, id }) {
+  if (typeof name === 'string') {
+    return getEmployeeByName(name);
+  }
+  if (typeof id === 'string') {
+    return getEmployeeById(id);
+  }
+  return undefined;
+}
+
+function getSpecies(ids) {
+  return ids.map((id) => species.find((animal) => animal.id === id));
+}
+
+function getSpeciesNames(animals) {
+  return animals.map(({ name }) => name);
+}
+
+function getSpeciesLocations(animals) {
+  return animals.map(({ location }) => location);
+}
+
+function getUniqueCoverage(employeeInfo) {
+  const employee = getEmployee(employeeInfo);
+
+  if (!employee) {
+    throw new Error('Informações inválidas');
+  }
+
+  const animals = getSpecies(employee.responsibleFor);
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: getSpeciesNames(animals),
+    locations: getSpeciesLocations(animals),
+  };
+}
+
+function getEmployeesCoverage(employeeInfo) {
+  if (!employeeInfo) {
+    return employees.map((employee) => getUniqueCoverage(employee));
+  }
+  return getUniqueCoverage(employeeInfo);
 }
 
 module.exports = getEmployeesCoverage;
