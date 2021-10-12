048b66f4fb20f067a5e6ef7746c90649eddf8069
Natã Elienai <nata.elienai@gmail.com>
2021-10-12 16:15:49 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,25 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
+
+function getEmployee(id) {
+  return employees.find((employee) => employee.id === id);
+}
+
+function getSpecies(id) {
+  return species.find((animal) => animal.id === id);
+}
+
+function getOldestResident(residents) {
+  return residents.reduce((oldestResident, currentResident) => (
+    currentResident.age > oldestResident.age ? currentResident : oldestResident
+  ));
+}
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { responsibleFor } = getEmployee(id);
+  const { residents } = getSpecies(responsibleFor[0]);
+  const oldestResident = getOldestResident(residents);
+
+  return Object.values(oldestResident);
 }
 
 module.exports = getOldestFromFirstSpecies;
